332e21a710efa0aa6c37818fd9f2acadbdf5a4c5
Kiran Prakash <kiran.in@gmail.com>
2011-10-27 11:25:20 +0530

    #1 | Initial commit, creating rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lysg::Application.config.secret_token = '79641c568c36ec95194bf17d5bddb4354fcde22b667535b4240051d0e85f7ddf24f8f2f21cf5b2cc1d6b2255b2c482cc96065fc60d643a4d84628a3f2d42fbc2'
